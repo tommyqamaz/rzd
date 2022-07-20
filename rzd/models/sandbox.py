39c3e279d107d5de6da594d3c191101bfea6ba17
tommyqamaz <solgrini@yandex.ru>
2022-07-20 12:44:30 +0300

    notebook3

--- rzd/models/sandbox.py
+++ rzd/models/sandbox.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 # IMAGE_EX_PATH = (
 #     "/home/qamaz/Documents/mlprojects/hackatons/rzd/img_0.0007435791908498857.png"
 # )
-IMAGE_EX_PATH = (
-    /home/qamaz/Documents/mlprojects/hackatons/rzd/rzd/data/sample_solution/img_0.1719878008870872.png
-)
+IMAGE_EX_PATH = "/home/qamaz/Documents/mlprojects/hackatons/rzd/rzd/data/sample_solution/img_0.1719878008870872.png"
 image = cv2.imread(str(IMAGE_EX_PATH), cv2.IMREAD_GRAYSCALE)
 plt.imshow(image)
 print(np.unique(image))
